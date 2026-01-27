1ac3d391ea13f9f242fb0fbbe01f5f03518f96c0
Rohit551800 <rohitsingh139915@gmail.com>
2026-01-28 01:21:45 +0530

    Window Objects in JS

---
+++ Windows/WindowObjectsTheory.js
@@ -0,0 +1,47 @@
+//Window 
+
+//Window is the main container, or we can say the global Object and any operations related to entire browser 
+// window can be a part of window object
+
+//All the members like objects, methods or properties .If they are the part of window object, then we do not 
+//refer the window object
+
+
+
+//Document
+
+//1:DOM is the child of window object
+
+//2:In the DOM we need to refer the documenet, if we want to use the document object , methods or properties
+
+//3:Document is just the object of the global object that is Window, which deals with the document of HTML elements themselves
+
+// ------------------>>Window Object :
+// The window object represents the global window in a browser.Both Browser Object Model(BOM) & Documenet Object model(DOM)
+// are the part of the window object 
+
+
+//BOM:The BOM represents the browser as an object and provides methods and properties for intereacting with the browser
+//itself(not directly related to the content of the web page).
+
+//EG: BOM features include window.navigator for browser navigation information , window.location for URL manipulation etc
+
+
+
+
+//DOM:The DOM represents the structured document as a tree of objects , where each object corresponds to a part of
+//the document (such as elements , attributes , and text).
+//DOM is focused on the content of the page , the BOM is focused on the browser Environment
+
+
+
+
+
+
+
+
+
+
+
+
+
