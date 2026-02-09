acf2098bfff444c4b6a65474205a12c3c6cb42a4
Rohit551800 <rohitsingh139915@gmail.com>
2026-02-09 22:03:11 +0530

    Todo Application Project

---
+++ JS ADV VID2/Local Storage/index.js
@@ -0,0 +1,14 @@
+//local Storage
+
+
+
+// --> localstorage.setItem(key_name , values);
+// --> localstorage.getItem(key_name);
+// --> localstorage.removeItem(key_name);
+
+
+//Json methods
+
+
+//JSON.stringify();
+//JSON.parse();
